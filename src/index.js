eacfded994a32463d6aa785b0f3995e37ece4b99
XiangZhifa <974688383@qq.com>
2020-04-02 18:01:53 +0800

    webpack中的 样式 相关配置

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 let str = require('./str');
-
+require('./index-style.css');
+require('./index-style.less');
 console.log(str);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // webpack 遵从 node 语法
 let path = require('path');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     devServer: {    //开发服务器的配置
         port: 8888,
@@ -24,6 +25,45 @@ module.exports = {
                 collapseWhitespace: true,   //将HTML代码压缩为一行
             },
             hash: true   //添加hash戳，避免缓存，每次生成新的文件
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: 'main.css', //将所有 css 抽出到 main.css 中
         })
-    ]
+    ],
+    module: { //模块
+        rules: [    //模块规则
+            {
+                test: /\.css$/,
+                use: [
+                    // {
+                    //     loader: 'style-loader',    // style-loader 把css插入到head标签中
+                    //     options: {
+                    //         //可以在其中对进行配置
+                    //     }
+                    // },
+                    MiniCssExtractPlugin.loader, //用来代替 style-loader
+                    'css-loader',   //配置css-loader 可以解析@import这种语法
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    // {
+                    //     loader: 'style-loader',    // style-loader 把css插入到head标签中
+                    //     options: {
+                    //         //可以在其中对进行配置
+                    //     }
+                    // },
+                    MiniCssExtractPlugin.loader, //用来代替 style-loader
+                    'css-loader',   //配置css-loader 可以解析@import这种语法
+                    'less-loader'   //.less -> .css
+                ]
+            },
+            //loader的特点是，功能专一
+            //loader的用法：用字符串，表示只用一个loader；使用多个loader，需要用 []
+            //loader的顺序：从右向左执行、从下向上执行
+            //loader还可以写成对象形式，可以用options进行配置
+        ],
+    }
 };
